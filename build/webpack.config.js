151598388fc51dc9a450d06c58e777fc729b835f
raohonghua <raohonghua@foxmail.com>
2019-02-24 21:44:26 +0800

    Initial commit

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,20 @@
 'use strict'
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../dist/index.html'),
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
